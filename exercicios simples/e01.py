e761875535e0726079a303db5b69340244a2588d
JonasBakunin <104414548+JonasBakunin@users.noreply.github.com>
2023-11-06 16:31:15 -0300

    Create e01.py

---
+++ exercicios simples/e01.py
@@ -0,0 +1,16 @@
+#Faça um programa capaz de realizar a leitura do seu nome completo e seu
+#curso. Imprima no console os respectivos inputs.
+#Entrada 
+
+#Informe seu nome: Maria Joaquina
+#Informe seu curso: Pedagogia
+
+#Saída
+
+#Olá, Maria Joaquina, seu curso é Pedagogia
+
+nome = input("Informe seu nome:")
+curso = input("Informe seu curso:")
+
+#print("Olá, " + nome + ", seu curso é " + curso);
+print(f"Olá, {nome}, seu curso é {curso}");
