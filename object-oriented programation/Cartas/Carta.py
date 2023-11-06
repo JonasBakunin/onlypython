51ef60e02f1eb15dce91911e093217805b89f353
JonasBakunin <104414548+JonasBakunin@users.noreply.github.com>
2023-11-06 15:47:46 -0300

    Create Carta.py

---
+++ object-oriented programation/Cartas/Carta.py
@@ -0,0 +1,10 @@
+class Carta:
+    def __init__(self, valor, naipe):
+        self.valor = valor
+        self.naipe = naipe
+
+    def get_valor(self):
+        return self.valor
+
+    def get_naipe(self):
+        return self.naipe
