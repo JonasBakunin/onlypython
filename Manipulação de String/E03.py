01610e6a3dfa0b51ede36b21b80998637d7fbf8d
JonasBakunin <104414548+JonasBakunin@users.noreply.github.com>
2023-11-06 16:14:26 -0300

    Create E03.py

---
+++ Manipulação de String/E03.py
@@ -0,0 +1,10 @@
+cadeia = 'ATCG'
+
+cadeia = cadeia.replace('A','t')
+cadeia = cadeia.replace('T','a')
+cadeia = cadeia.replace('C','g')
+cadeia = cadeia.replace('G', 'c')
+
+cadeia = cadeia.upper()
+
+print(cadeia)
