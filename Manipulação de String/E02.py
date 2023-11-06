73204583e784ff3a9075c9d3c35894c9494c85ce
JonasBakunin <104414548+JonasBakunin@users.noreply.github.com>
2023-11-06 16:13:05 -0300

    Create E02.py

---
+++ Manipulação de String/E02.py
@@ -0,0 +1,9 @@
+#Faça um programa em Python que leia uma data de nascimento no formato dd/mm/aaaa e imprima a data com o mês escrito por extenso.
+dic = {'01':'Janeiro','02':'Fevereiro','03':'Março', '04':'Abril', '05':'Maio', '06':'Junho','07':'Julho',
+       '08':'Agosto', '09':'Setembro','10':'Outubro','11':'Novembro','12':'Dezembro'}
+
+data = input('Informe a data no formatado dd/mm/aaaa : ')
+data = data.split('/')
+
+
+print(f'Você nasceu em {data[0]} de {dic[data[1]]} de {data[2]}')
