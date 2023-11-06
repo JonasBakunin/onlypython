6dfae6bf157fd5412631ecc4a54276ae6059cbfe
JonasBakunin <104414548+JonasBakunin@users.noreply.github.com>
2023-11-06 16:13:17 -0300

    Update E02.py

--- Manipulação de String/E02.py
+++ Manipulação de String/E02.py
@@ -1,4 +1,5 @@
 #Faça um programa em Python que leia uma data de nascimento no formato dd/mm/aaaa e imprima a data com o mês escrito por extenso.
+
 dic = {'01':'Janeiro','02':'Fevereiro','03':'Março', '04':'Abril', '05':'Maio', '06':'Junho','07':'Julho',
        '08':'Agosto', '09':'Setembro','10':'Outubro','11':'Novembro','12':'Dezembro'}
 
